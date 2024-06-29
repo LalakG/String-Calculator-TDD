28f8e380d65601faa9247c1a17b5ab884521cb52
LalakG <lalakgarg2806@gmail.com>
2024-06-30 04:00:03 +0530

    Add a class and function to execute code

---
+++ string_calculator.rb
@@ -0,0 +1,44 @@
+# string_calculator.rb
+
+class StringCalculator
+  def self.add(string_numbers)
+    return 0 if string_numbers.empty?
+
+    delimiter, string_numbers = extract_delimiter(string_numbers)
+    parts = split_numbers(string_numbers, delimiter)
+    integers = parts.map(&:to_i)
+
+    raise_for_negatives(integers)
+
+    integers.sum
+  end
+
+  private
+
+  def self.extract_delimiter(string_numbers)
+    if string_numbers.start_with?('//')
+      parts = string_numbers.split("\n", 2)
+      delimiter = extract_custom_delimiter(parts[0][2..-1])
+      [delimiter, parts[1]]
+    else
+      [',', string_numbers]
+    end
+  end
+
+  def self.extract_custom_delimiter(delimiter_part)
+    if delimiter_part.start_with?('[') && delimiter_part.end_with?(']')
+      delimiter_part.scan(/\[(.*?)\]/).join('|')
+    else
+      delimiter_part
+    end
+  end
+
+  def self.split_numbers(string_numbers, delimiter)
+    string_numbers.split(/#{Regexp.quote(delimiter)}|\n/)
+  end
+
+  def self.raise_for_negatives(integers)
+    negatives = integers.select { |n| n < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+  end
+end
